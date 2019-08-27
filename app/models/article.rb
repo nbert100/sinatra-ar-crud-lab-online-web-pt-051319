5ce78a31f47c15dd1f99eb778e84de37a0ec45fc
Nia Bert <nbert14@gmail.com>
2019-08-27 22:11:24 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,6 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-  attr_reader :title, :content
-  
-  def initialize(params)
-    @title = params[:title]
-    @content = params[:content]
-  end
+ 
   
 end
