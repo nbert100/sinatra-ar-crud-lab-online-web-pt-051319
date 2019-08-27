bdb2da09535059e7c979c637f98f21c6d427259c
Nia Bert <nbert14@gmail.com>
2019-08-27 23:24:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,7 +48,7 @@ class ApplicationController < Sinatra::Base
   
   delete '/articles/:id' do
     @article = Article.find(params[:id])
-    @article.delete
+    @article.destroy
     erb :index
   end 
 end
